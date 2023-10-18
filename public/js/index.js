eede376772a151880f54a4fdac34bae52d6c4cd2
dkiprianou <dkiprianou@olympia-electronics.gr>
2023-10-18 12:39:46 +0300

    print both forms

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,43 @@
 let newFormName = "";
 let editFormName = "form_1";
 let dataTable = null;
-let flatpickrInstance = null;
 let rowdata = "";
 
 window.onload = function () {
   footer();
   EditForm(editFormName);
-  document.getElementById("registered-forms").addEventListener("change", function () {
-      editFormName = this.value;
-      EditForm(editFormName);
-    });
-
-  document.getElementById("create-forms").addEventListener("change", function () {
-      newFormName = this.value;
-      fetchClearHtml(); 
-    });
-
-  document.addEventListener("click", function (e) {
-    if (!e.target.closest("#formModal .modal-content")) {
-      $("#formModal").modal("hide");
-    }
-  });
-
-  document.addEventListener("keyup", function (e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-      $("#formModal").modal("hide");
-    }
-  });
+  setupEventListeners();
 };
 
+function setupEventListeners() {
+  document.getElementById("registered-forms").addEventListener("change", handleFormChange);
+  document.getElementById("create-forms").addEventListener("change", handleNewFormChange);
+
+  document.addEventListener("click", closeModalOnOutsideClick);
+  document.addEventListener("keyup", closeModalOnEscape);
+}
+
+function handleFormChange() {
+  editFormName = this.value;
+  EditForm(editFormName);
+}
+
+function handleNewFormChange() {
+  newFormName = this.value;
+  fetchClearHtml();
+}
+
+function closeModalOnOutsideClick(e) {
+  if (!e.target.closest("#formModal .modal-content")) {
+    $("#formModal").modal("hide");
+  }
+}
+
+function closeModalOnEscape(e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    $("#formModal").modal("hide");
+  }
+}
 //edit forms
 function EditForm(formName) {
   fetch(`load-form-data`, {
@@ -44,37 +52,40 @@ function EditForm(formName) {
       return res.json();
     })
     .then((data) => {
-      let columns = [];
-
-      for (let key in data[0]) {
-        columns.push({ data: key, title: key });
-      }
-
-      if (dataTable) {
-        $("#datatable").DataTable().destroy();
-        $("#datatable").empty();
-      }
-
-      dataTable = $("#datatable").DataTable({
-        data: data,
-        columns: columns,
-        scrollX: true,
-        lengthMenu: [5, 10, 20, 50, 100],
-      });
-
-      $("#datatable tbody").on("dblclick", "tr", async function () {
-        rowdata = await dataTable.row(this).data();
-        $("#formModal").modal("show");
-      });
-
-      $("#formModal").on("show.bs.modal", function () {
-        document.getElementById("create-forms").selectedIndex = 0;
-        document.querySelector(".form-container").innerHTML = "";
-        fetchHtml();
-      });
-
+      renderDataTable(data);
     })
     .catch((err) => console.error(`Error submitting form: ${err}`));
+}
+
+function renderDataTable(data) {
+  let columns = [];
+
+  for (let key in data[0]) {
+    columns.push({ data: key, title: key });
+  }
+
+  if (dataTable) {
+    $("#datatable").DataTable().destroy();
+    $("#datatable").empty();
+  }
+
+  dataTable = $("#datatable").DataTable({
+    data: data,
+    columns: columns,
+    scrollX: true,
+    lengthMenu: [5, 10, 20, 50, 100],
+  });
+
+  $("#datatable tbody").on("dblclick", "tr", async function () {
+    rowdata = await dataTable.row(this).data();
+    $("#formModal").modal("show");
+  });
+
+  $("#formModal").on("show.bs.modal", function () {
+    document.getElementById("create-forms").selectedIndex = 0;
+    document.querySelector(".form-container").innerHTML = "";
+    fetchHtml();
+  });
 }
 
 function fetchHtml() {
@@ -103,15 +114,15 @@ function updateForm() {
     submitForm2(rowdata.Id);
   }
   $("#formModal").modal("hide");
-
 }
 
 //create new forms
 function fetchClearHtml() {
-  let exportBtn = document.getElementById("export-btn");
-  let submitBtn = document.getElementById("submit-btn");
+  const exportBtn = document.getElementById("export-btn");
+  const submitBtn = document.getElementById("submit-btn");
   exportBtn.removeAttribute("disabled");
   submitBtn.removeAttribute("disabled");
+
   if (newFormName === "") {
     document.querySelector(".form-container").innerHTML = "";
   } else {
@@ -133,8 +144,8 @@ function submitForm() {
   }
 }
 
-async function exportPdf() {
-  if (newFormName === "form_1") {
+function exportPdf() {
+  if (newFormName === "form_1" || editFormName === "form_1") {
     let options = {
       filename: "Form One.pdf",
       image: { type: "jpeg", quality: 1 },
@@ -144,7 +155,7 @@ async function exportPdf() {
 
     let form1 = document.getElementById("form-1");
     html2pdf().set(options).from(form1).save();
-  } else if (newFormName === "form_2") {
+  } else if (newFormName === "form_2" || editFormName === "form_2") {
     let options = {
       filename: "Form Two.pdf",
       image: { type: "jpeg", quality: 1 },
